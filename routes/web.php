da34b34ee60058e93bac3afc6543063111f09e51
axxeldevanda <axxeldevanda@gmail.com>
2020-07-02 16:15:17 +0700

    Templating frontend (index)

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
+
+
 Route::prefix('admin',)
     ->namespace('Admin')
     ->group(function(){Route::get('/','DashboardController@index')
