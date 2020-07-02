d0477705233074358aaf8995332abe5573703b68
axxeldevanda <axxeldevanda@gmail.com>
2020-07-02 16:31:36 +0700

    Templating  frontend (Details)

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index');
 Route::get('checkout', 'CheckoutController@index');
+Route::get('details', 'DetailsController@index');
 
 
 Route::prefix('admin',)
