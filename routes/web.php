9bebe009f3c909838fdbcefacdafbd92e2882b56
axxeldevanda <axxeldevanda@gmail.com>
2020-07-02 15:51:13 +0700

    Templating Admin

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::prefix('admin',)
+    ->namespace('Admin')
+    ->group(function(){Route::get('/','DashboardController@index')
+    ->name('dashboard');
+});
