fbd5626c9ee72ff9810de41327bd49aafff83279
axxeldevanda <axxeldevanda@gmail.com>
2020-07-03 10:22:57 +0700

    make migration galleries

--- database/migrations/2020_07_03_032221_create_transactions_table.php
+++ database/migrations/2020_07_03_032221_create_transactions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateGalleriesTable extends Migration
+class CreateTransactionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateGalleriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('galleriess', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->integer('travel_packages_id');
-            $table->text('image');
-            $table->softDeletes();
+        Schema::create('transactionss', function (Blueprint $table) {
+            $table->id();
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ class CreateGalleriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('galleriess');
+        Schema::dropIfExists('transactionss');
     }
 }
