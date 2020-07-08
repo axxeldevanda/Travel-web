992fc2f111691a27be3a05c8094e1c17c0e06f60
axxeldevanda <axxeldevanda@gmail.com>
2020-07-08 11:05:29 +0700

    laravel auth

--- database/migrations/2020_07_03_032722_create_transaction_details_table.php
+++ database/migrations/2020_07_03_032722_create_transaction_details_table.php
@@ -14,7 +14,14 @@ class CreateTransactionDetailsTable extends Migration
     public function up()
     {
         Schema::create('transaction_details', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->integer('transactions_id');
+            $table->string('username');
+            $table->string('nationality');
+            $table->boolean('is_visa');
+            $table->date('doe_passport');
+            $table->softDeletes();
+
             $table->timestamps();
         });
     }
